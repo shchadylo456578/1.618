b85cdf6a321f1b265c10c40b09824c01be9dc893
shchadylo456578 <tarasdidg@gmail.com>
2017-10-27 21:01:52 +0300

    add change text main page

--- js/scripts.js
+++ js/scripts.js
@@ -57,6 +57,16 @@
     $('#navbartitle').click(function(e){
         $('#navbar').show(500);
         $('#navbartitle').hide(500);
+
+        var dotsOpacity = $('#dotsAnimate').css("opacity");
+
+        console.log(dotsOpacity);
+
+        if(dotsOpacity < 1) {
+          $('#dotsAnimate').css({opacity: 1})
+        }
+
+
     });
 
     /**
@@ -76,50 +86,41 @@
 })();
 
 
+var words = [
+  'Ви зростаєте <br/> Ми втілюємо',
+  'Ви відпочиваєте <br/> Ми працюємо',
+  'Ви спите <br/> Ми ремонтуємо',
+  'Ви радієте <br/> Ми будуємо'
+];
+
+$(function () {
+  count = 0;
+  setInterval(function () {
+    count++;
+    $("#changeWords").fadeOut(500, function () {
+      $(this).html(words[count % words.length]).fadeIn(500);
+    });
+  }, 3000);
+});
+
 $(document).ready(function(){
 
   function freshDot(){
     this.obj = document.createElement("div");
-
     this.obj.classList.add("box");
-
     var container = $('#dotsAnimate');
-
-
-    // console.log(container.innerHeight);
-    // console.log(container.innerHeight*Math.random());
-
 
     this.obj.style.top = (container.height() * Math.random()) + 'px';
     this.obj.style.left = (container.width() * Math.random()) + 'px';
 
-    // this.size = Math.floor(0 * Math.random()) + 8;
-    // this.size = 8;
-
-    // this.obj.style.height =  this.size + 'px';
-    // this.obj.style.width = this.size + 'px';
-
-    // document.body.appendChild(this.obj);  //
-    // console.log(this.obj);
-
     container.append(this.obj);
   }
 
-//   freshDot();
   var dot = [];
 
   for(var i = 0 ; i < 9 ; i++ ){
     dot.push(new freshDot());
   }
-
-
-  /*
-  $(window).resize(function(){
-    for(i=0;i<200;i++){
-      document.body.removeChild(dot[i]);
-    }
-  });
-  */
 });
 
 $(window, document, undefined).ready(function() {

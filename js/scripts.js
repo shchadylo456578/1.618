668014f62e5258a43782d7a937603fcf2706db3b
shchadylo456578 <tarasdidg@gmail.com>
2017-11-01 11:44:43 +0200

    Merge branch 'animation' of https://github.com/shchadylo456578/1618-full into animation
    # Conflicts:
    #	src/js/slider.js
    #	src/style/pages/_main-page.scss

--- js/scripts.js
+++ js/scripts.js
@@ -428,53 +428,60 @@ var settings = {
 smartgrid('style/modules', settings);
 
 
-var btn_prev = document.getElementById('prev');
-var btn_next = document.getElementById('next');
-var images = document.getElementsByClassName('image-slide');
-var i = 0;
+
+$(document).ready(function(){
+  var owl = $('.owl-carousel');
+
+  owl.owlCarousel({
+    stagePadding: '50',
+    touchDrag  : false,
+    mouseDrag  : false,
+    smartSpeed:1000,
+    animateOut: 'bringOut',
+    animateIn: 'bringIn',
+    loop:false,
+    margin: 15,
+    nav:true,
+    navText:['<div id="prev">Попередній<br>проект</div>','<div id="next">Наступний<br>проект</div>'],
+    responsive: {
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+  });
+
+  owl.on('change.owl.carousel', function(event) {
+
+    var $currentItem = $('.owl-item').eq(event.item.index);
+    var $itemToShow = $('.owl-item').eq(event.property.value);
+
+    if(event.item.index < event.property.value){
+      $itemToShow.addClass('animated owl-animated-in fadeNext');
+    }
+
+    if(event.item.index > event.property.value){
+      $currentItem.addClass('animated owl-animated-in fadePrev');
+      $($itemToShow).css({left:'0'});
+    }
 
 
-btn_next.addEventListener('click', function() {
-  images[i].classList.remove("active");
-  i--;
-  if(i < 0) {
-    i = images.length - 1;
-  }
-  images[i].classList.add('active');
-  TweenMax.fromTo(".active", 1, {scale: 1.1, boxShadow:"25px 0px 10px 20px rgba(0,0,0,0.59)", zIndex:100 }, {x: 800, scale:1,  boxShadow:"0px 0px 0px 0px rgba(0,0,0,0.59)" });
+  });
+
+  owl.on('translated.owl.carousel', function(event) {
+
+    $('.owl-item').removeClass('animated fadePrev fadeNext bringOut bringIn owl-animated-out owl-animated-in');
+    $('.owl-item.active').css({left:'0'});
+  });
 
 
 });
 
-btn_prev.addEventListener('click', function() {
-
-  images[i].classList.remove("active");
-  i++;
-  //console.log(i);
-  if(i >= images.length) {
-    i = 0;
-  }
-  images[i].classList.add('active');
-  TweenMax.fromTo(".active", 1, {scale: 1.1, boxShadow:"25px 0px 10px 20px rgba(0,0,0,0.59)", zIndex:100 }, {x: 0, scale:1,  boxShadow:"0px 0px 0px 0px rgba(0,0,0,0.59)"});
-
-});
-
-
-// (function($) {
-//   var btn_prev = $('#prev');
-//   var btn_next = $('#next');
-//   var images = $('.image-slide');
-//   var i = 0;
-//
-//   $(btn_prev).click(function () {
-//     TweenMax.fromTo(".active", 1, {scale: 1.1, boxShadow:"0px 0px 40px 20px rgba(0,0,0,0.59)", zIndex:100 }, {x: 650, scale:1,  boxShadow:"0px 0px 0px 0px rgba(0,0,0,0.59)"});
-//
-//   });
-//   $(btn_next).click(function () {
-//     TweenMax.fromTo(".active", 1, {scale: 1.1, boxShadow:"0px 0px 40px 20px rgba(0,0,0,0.59)", zIndex:100 }, {x: 0, scale:1,  boxShadow:"0px 0px 0px 0px rgba(0,0,0,0.59)"});
-//
-//   });
-// })(jQuery);
 
 
 

4bdfd460e9cff1177e61079bc9eb8a55ae562c82
shchadylo456578 <tarasdidg@gmail.com>
2017-11-14 12:46:12 +0200

    add pages_article for word-press

--- js/scripts.js
+++ js/scripts.js
@@ -97,22 +97,61 @@
 // })();
 
 
-var words = [
-  'Ви зростаєте <br/> Ми втілюємо',
-  'Ви відпочиваєте <br/> Ми працюємо',
-  'Ви спите <br/> Ми ремонтуємо',
-  'Ви радієте <br/> Ми будуємо'
+var wordsfirsr = [
+  ' зростаєте',
+  ' відпочиваєте',
+  ' спите',
+  ' радієте',
+  ' мрієте'
 ];
 
 $(function () {
   count = 0;
   setInterval(function () {
     count++;
-    $("#changeWords").fadeOut(500, function () {
-      $(this).html(words[count % words.length]).fadeIn(500);
+    $("#changeWords_1").fadeOut(500, function () {
+      $(this).html(wordsfirsr[count % wordsfirsr.length]).fadeIn(500);
     });
   }, 3000);
 });
+
+
+var wordssecond = [
+  ' втілюємо',
+  ' працюємо',
+  ' ремонтуємо',
+  ' будуємо',
+  ' проектуємо'
+];
+
+$(function () {
+  count = 0;
+  setInterval(function () {
+    count++;
+    $("#changeWords_2").fadeOut(500, function () {
+      $(this).html(wordssecond[count % wordssecond.length]).fadeIn(500);
+    });
+  }, 3000);
+});
+
+
+
+// var words = [
+//   'Ви зростаєте <br/> Ми втілюємо',
+//   'Ви відпочиваєте <br/> Ми працюємо',
+//   'Ви спите <br/> Ми ремонтуємо',
+//   'Ви радієте <br/> Ми будуємо'
+// ];
+//
+// $(function () {
+//   count = 0;
+//   setInterval(function () {
+//     count++;
+//     $("#changeWords").fadeOut(500, function () {
+//       $(this).html(words[count % words.length]).fadeIn(500);
+//     });
+//   }, 3000);
+// });
 
 $(document).ready(function(){
 
@@ -127,38 +166,38 @@ $(document).ready(function(){
   }
 
   var controller = new ScrollMagic.Controller();
-  var arrow = new TimelineMax;
+//   var arrow = new TimelineMax;
 
 
-  var arrowStartHeight = ($('.header-text').height() - 40) +'px';
-  var arrowEndHeight = ($(window).height()-$('#dotsAnimate').height())+'px';
-  var arrowBlackEnd = $('.footer').position().top+'px';
+//   var arrowStartHeight = ($('.header-text').height() - 40) +'px';
+//   var arrowEndHeight = ($(window).height()-$('#dotsAnimate').height())+'px';
+//   var arrowBlackEnd = $('.footer').position().top+'px';
 
 
 
-  arrow
-    .from('.logo-animate', 0, {height:arrowStartHeight})
-    .to('.logo-animate', 1, {height:arrowEndHeight},0)
-    ;
+//   arrow
+//     .from('.logo-animate', 0, {height:arrowStartHeight})
+//     .to('.logo-animate', 1, {height:arrowEndHeight},0)
+//     ;
 
-  var logoPinScene = new ScrollMagic.Scene({
-    triggerHook:0,
-    offset:'20%'
-  })
-  .setTween(arrow)
-  .setPin('.logo-pos',{pushFollowers:false})
-  .addTo(controller)
-  .addIndicators();
+//   var logoPinScene = new ScrollMagic.Scene({
+//     triggerHook:0,
+//     offset:'20%'
+//   })
+//   .setTween(arrow)
+//   .setPin('.logo-pos',{pushFollowers:false})
+//   .addTo(controller)
+//   .addIndicators();
 
-  var logofooterScene = new ScrollMagic.Scene({
-    triggerElement:'.footer',
-    offset:'172'
+//   var logofooterScene = new ScrollMagic.Scene({
+//     triggerElement:'.footer',
+//     offset:'172'
 
-    // duration:'20%'
-  })
-  .setPin('#logofooter',{pushFollowers:false})
-  .addTo(controller)
-  .addIndicators();
+//     // duration:'20%'
+//   })
+//   .setPin('#logofooter',{pushFollowers:false})
+//   .addTo(controller)
+//   .addIndicators();
 
 
   var menu_bottom_line_show = $('.footer').position().top - $(window).height();
@@ -180,7 +219,7 @@ $(document).ready(function(){
       $('.navbar').toggleClass('fade-out');
     }
 
-    console.log(event.scrollDirection);
+    // console.log(event.scrollDirection);
   });
 
       /*
@@ -236,11 +275,14 @@ $(document).ready(function(){
     menu_bottom_line_show = $('.footer').position().top - $(window).height();
     navbartitle.duration(menu_bottom_line_show);
 
-    console.log('dur!!',navbartitle.duration());
+    // console.log('dur!!',navbartitle.duration());
+    
+    $('.footer .logo-animate').height($('.footer .footer-logo-down').position().top + $('.footer .logo-animate').width());
 
     var dot = [];
 
     var container = $('#dotsAnimate');
+
     container.text('');
 
     for(var i = 0 ; i < 9 ; i++ ){
